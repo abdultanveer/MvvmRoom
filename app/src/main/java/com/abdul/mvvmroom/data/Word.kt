687277b1f49d0680902fce6afca8b3430166cfb2
abdul <abdul.tanveer@gmail.com>
2022-01-29 11:17:38 +0530

    student java class

--- app/src/main/java/com/abdul/mvvmroom/data/Word.kt
+++ app/src/main/java/com/abdul/mvvmroom/data/Word.kt
@@ -1,8 +1,12 @@
 package com.abdul.mvvmroom.data
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 //data class is a model or POJO - plain old java object, VO[one value]
 
 @Entity(tableName = "word_table")
-data class Word(val word: String)
+data class Word
+                (@PrimaryKey (autoGenerate = true) val id: Int,
+                @ColumnInfo(name = "word")val word: String)
